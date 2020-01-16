5a115ef5207992a540ce47e01eced88473000244
526077247 <45992829+526077247@users.noreply.github.com>
2020-01-16 22:49:01 +0800

    添加注释自动展示

--- service.core/Core/AssxHelper.cs
+++ service.core/Core/AssxHelper.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace service.core
 {
@@ -25,11 +29,25 @@ namespace service.core
                 string IntfAss = ConfigurationManager.Configuration["Services:" + SvrID + ":IntfAssembly"];
                 string IntfName = ConfigurationManager.Configuration["Services:" + SvrID + ":IntfName"];
                 Type intf = ServiceManager.GetTypeFromAssembly(IntfName, Assembly.Load(IntfAss));
+                string notePath = AppDomain.CurrentDomain.BaseDirectory + "/" + IntfAss + ".xml";
+                List<XElement> elements = new List<XElement>();
+                if (File.Exists(notePath))
+                {
+                    XElement xe = XElement.Load(notePath);
+                    elements.AddRange(xe.Elements("members").Elements("member"));
+                              
+                }
+                if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + "/service.core.xml"))
+                {
+                    XElement xe = XElement.Load(AppDomain.CurrentDomain.BaseDirectory + "/service.core.xml");
+                    elements.AddRange(xe.Elements("members").Elements("member"));
+
+                }
                 if (intf != null)
                 {
                     StringBuilder builder = new StringBuilder();
                     builder.AppendLine("服务接口定义：");
-                    builder.Append(GetSvrStr(intf));
+                    builder.Append(GetSvrStr(intf, elements));
                     result = builder.ToString();
                 }
                 else
@@ -43,18 +61,19 @@ namespace service.core
         /// 取服务接口列表
         /// </summary>
         /// <param name="intf"></param>
+        /// <param name="notes"></param>
         /// <returns></returns>
-        public static StringBuilder GetSvrStr(Type intf)
+        public static StringBuilder GetSvrStr(Type intf, List<XElement> notes)
         {
             StringBuilder builder = new StringBuilder();
             foreach (Type type in intf.GetInterfaces())
             {
-                builder.Append(GetSvrStr(type));
+                builder.Append(GetSvrStr(type, notes));
             }
             foreach (MethodInfo Method in intf.GetMethods())
             {
                 builder.AppendLine();
-                builder.AppendLine(GetSvrMethodStr(Method));
+                builder.AppendLine(GetSvrMethodStr(Method, notes));
             }
             
             return builder;
@@ -63,14 +82,16 @@ namespace service.core
         /// 取方法
         /// </summary>
         /// <param name="method"></param>
+        /// <param name="notes"></param>
         /// <returns></returns>
-        public static string GetSvrMethodStr(MethodInfo method)
+        public static string GetSvrMethodStr(MethodInfo method, List<XElement> notes)
         {
             if(method.GetCustomAttribute(typeof(PublishMethodAttribute))==null)
             {
                 return "";
             }
             StringBuilder builder = new StringBuilder();
+            builder.Append(GetSvrMethodNotes(method, notes));
             builder.Append(method.ReturnType.Name + " ");
             builder.Append(method.Name+"(");
             StringBuilder para = new StringBuilder();
@@ -83,6 +104,30 @@ namespace service.core
             else
                 builder.Append(");");
             return builder.ToString();
+        }
+
+        /// <summary>
+        /// 取注释
+        /// </summary>
+        /// <param name="svrID"></param>
+        /// <param name="method"></param>
+        /// <param name="notes"></param>
+        /// <returns></returns>
+        public static string GetSvrMethodNotes(MethodInfo method, List<XElement> notes)
+        {
+            foreach(var item in notes)
+            {
+                if(item.Attribute("name").ToString().Contains(method.Name)&&item.Attribute("name").ToString().Contains(method.Module.Assembly.GetName().Name))
+                {
+                    StringBuilder SB = new StringBuilder();
+                    foreach(var node in item.Elements())
+                    {
+                        SB.AppendLine(node.ToString().Replace(" ",""));
+                    }
+                    return SB.ToString();
+                }
+            }
+            return "";
         }
     }
 }
