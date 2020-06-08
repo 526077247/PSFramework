238cd1917c186ea6eebaa06ed7fe91bbefadafd4
526077247 <45992829+526077247@users.noreply.github.com>
2020-06-08 11:02:15 +0800

    补充提交

---
+++ service.core/Domain/ResultList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace service.core
+{
+    public class ResultList<T>
+    {
+        public List<T> list { get; set; }
+        public int total { get; set; }
+        public int pageSize { get; set; }
+        public int start { get; set; }
+    }
+}
